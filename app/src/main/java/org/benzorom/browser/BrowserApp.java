pixeldustproject-caf/android_packages_apps_Browser

Browser: allow disabling clear text traffic at runtime
Change-Id: I6ea63aeb8f6264aec7fe5aea5fa90f167c886677
Signed-off-by: Joey <joey@lineageos.org>
Signed-off-by: spezi77 <spezi7713@gmx.net>

java/com/pixeldust/browser/BrowserApp.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pixeldust.browser;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.pixeldust.browser.utils.NetworkSecurityPolicyUtils;
+
+public class BrowserApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        setClearTextTrafficConfig();
+    }
+
+    private void setClearTextTrafficConfig() {
+        if (!NetworkSecurityPolicyUtils.isSupported()) {
+            return;
+        }
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isPermitted = prefs.getBoolean("key_clear_text_traffic", true);
+        NetworkSecurityPolicyUtils.setCleartextTrafficPermitted(isPermitted);
+    }
+}

java/com/pixeldust/browser/SettingsActivity.java
@@ -19,6 +19,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
 import android.support.v4.content.LocalBroadcastManager;
@@ -33,6 +34,7 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.pixeldust.browser.utils.IntentUtils;
+import com.pixeldust.browser.utils.NetworkSecurityPolicyUtils;
 import com.pixeldust.browser.utils.PrefsUtils;
 import com.pixeldust.browser.utils.UiUtils;
 
@@ -56,6 +58,9 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstance) {
             super.onCreate(savedInstance);
             addPreferencesFromResource(R.xml.settings);
+
+            PreferenceCategory securityCategory = (PreferenceCategory)
+                    findPreference("category_security");
 
             Preference homePage = findPreference("key_home_page");
             homePage.setSummary(PrefsUtils.getHomePage(getContext()));
@@ -82,6 +87,17 @@ public class SettingsActivity extends AppCompatActivity {
                     LocalBroadcastManager.getInstance(getContext()).sendBroadcast(intent);
                     return true;
                 });
+            }
+
+            SwitchPreference clearTextTraffic = (SwitchPreference)
+                    findPreference("key_clear_text_traffic");
+            if (NetworkSecurityPolicyUtils.isSupported()) {
+                clearTextTraffic.setOnPreferenceChangeListener((preference, value) -> {
+                    NetworkSecurityPolicyUtils.setCleartextTrafficPermitted((Boolean) value);
+                    return true;
+                });
+            } else {
+                securityCategory.removePreference(clearTextTraffic);
             }
         }
 

BenzoRom/packages_apps_Browser

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

app/src/main/java/org/benzorom/browser/BrowserApp.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.benzorom.browser;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.benzorom.browser.utils.NetworkSecurityPolicyUtils;
+
+public class BrowserApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        setClearTextTrafficConfig();
+    }
+
+    private void setClearTextTrafficConfig() {
+        if (!NetworkSecurityPolicyUtils.isSupported()) {
+            return;
+        }
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isPermitted = prefs.getBoolean("key_clear_text_traffic", true);
+        NetworkSecurityPolicyUtils.setCleartextTrafficPermitted(isPermitted);
+    }
+}

app/src/main/java/org/benzorom/browser/SettingsActivity.java
@@ -19,6 +19,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
 import android.support.v4.content.LocalBroadcastManager;
@@ -33,6 +34,7 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import org.benzorom.browser.utils.IntentUtils;
+import org.benzorom.browser.utils.NetworkSecurityPolicyUtils;
 import org.benzorom.browser.utils.PrefsUtils;
 import org.benzorom.browser.utils.UiUtils;
 
@@ -56,6 +58,9 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstance) {
             super.onCreate(savedInstance);
             addPreferencesFromResource(R.xml.settings);
+
+            PreferenceCategory securityCategory = (PreferenceCategory)
+                    findPreference("category_security");
 
             Preference homePage = findPreference("key_home_page");
             homePage.setSummary(PrefsUtils.getHomePage(getContext()));
@@ -82,6 +87,17 @@ public class SettingsActivity extends AppCompatActivity {
                     LocalBroadcastManager.getInstance(getContext()).sendBroadcast(intent);
                     return true;
                 });
+            }
+
+            SwitchPreference clearTextTraffic = (SwitchPreference)
+                    findPreference("key_clear_text_traffic");
+            if (NetworkSecurityPolicyUtils.isSupported()) {
+                clearTextTraffic.setOnPreferenceChangeListener((preference, value) -> {
+                    NetworkSecurityPolicyUtils.setCleartextTrafficPermitted((Boolean) value);
+                    return true;
+                });
+            } else {
+                securityCategory.removePreference(clearTextTraffic);
             }
         }
 